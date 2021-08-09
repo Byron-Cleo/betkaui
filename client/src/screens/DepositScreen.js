430efe193740f0fbdc19ee09c104fcae4cfdfa6d
Byron-Cleo <byron.ochara@gmail.com>
2021-08-09 17:28:53 +0300

    Finished deposite part one

--- client/src/screens/DepositScreen.js
+++ client/src/screens/DepositScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container, Card, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFutbol, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const DepositScreen = () => {
   return (
@@ -31,6 +33,399 @@ const DepositScreen = () => {
           <h3 className="navbar-brand">Away</h3>
         </Col>
       </Row>
+      <div>
+        <Card
+          border="light"
+          style={{
+            backgroundColor: '#0c1625',
+            height: '2.5rem',
+            paddingLeft: '2rem',
+            paddingRight: '2rem',
+            border: '#0c1625',
+          }}
+        >
+          <Card.Header
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: '#ABADB2',
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faFutbol}
+                style={{
+                  width: '1.5rem',
+                  height: '1.5rem',
+                  marginRight: '10px',
+                }}
+              />
+              England
+              <FontAwesomeIcon
+                icon={faCircle}
+                style={{
+                  width: '.5rem',
+                  height: '.5rem',
+                  marginLeft: '10px',
+                  marginRight: '10px',
+                  alignSelf: 'center',
+                }}
+              />
+              Championship
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: '#ABADB2',
+              }}
+            >
+              04/05, 22:00
+            </div>
+          </Card.Header>
+          <Card.Body
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Card.Title>
+              <div>Luton</div>
+              <div>Rotherham</div>
+            </Card.Title>
+            <Card.Text>
+              <div
+                style={{
+                  backgroundColor: '#1A2E37',
+                  border: 'none',
+                  pointer: 'cursor',
+                  padding: '.2rem',
+                  width: '100%',
+                }}
+              >
+                <div
+                  style={{
+                    padding: '.2rem',
+                  }}
+                >
+                  Market: Both Teams to Score
+                </div>
+                <div
+                  style={{
+                    padding: '.2rem',
+                    color: 'green',
+                  }}
+                >
+                  Pick: Yes
+                </div>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card
+          border="light"
+          style={{
+            backgroundColor: '#0c1625',
+            height: '2.5rem',
+            paddingLeft: '2rem',
+            paddingRight: '2rem',
+            border: '#0c1625',
+            marginTop: '5rem',
+          }}
+        >
+          <Card.Header
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: '#ABADB2',
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faFutbol}
+                style={{
+                  width: '1.5rem',
+                  height: '1.5rem',
+                  marginRight: '10px',
+                }}
+              />
+              England
+              <FontAwesomeIcon
+                icon={faCircle}
+                style={{
+                  width: '.5rem',
+                  height: '.5rem',
+                  marginLeft: '10px',
+                  marginRight: '10px',
+                  alignSelf: 'center',
+                }}
+              />
+              Championship
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: '#ABADB2',
+              }}
+            >
+              04/05, 22:00
+            </div>
+          </Card.Header>
+          <Card.Body
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Card.Title>
+              <div>Luton</div>
+              <div>Rotherham</div>
+            </Card.Title>
+            <Card.Text>
+              <div
+                style={{
+                  backgroundColor: '#1A2E37',
+                  border: 'none',
+                  pointer: 'cursor',
+                  padding: '.2rem',
+                  width: '100%',
+                }}
+              >
+                <div
+                  style={{
+                    padding: '.2rem',
+                  }}
+                >
+                  Market: 3 Away
+                </div>
+                <div
+                  style={{
+                    padding: '.2rem',
+                    color: 'green',
+                  }}
+                >
+                  Pick: Luton
+                </div>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card
+          border="light"
+          style={{
+            backgroundColor: '#0c1625',
+            height: '2.5rem',
+            paddingLeft: '2rem',
+            paddingRight: '2rem',
+            border: '#0c1625',
+            marginTop: '5rem',
+          }}
+        >
+          <Card.Header
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: '#ABADB2',
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faFutbol}
+                style={{
+                  width: '1.5rem',
+                  height: '1.5rem',
+                  marginRight: '10px',
+                }}
+              />
+              England
+              <FontAwesomeIcon
+                icon={faCircle}
+                style={{
+                  width: '.5rem',
+                  height: '.5rem',
+                  marginLeft: '10px',
+                  marginRight: '10px',
+                  alignSelf: 'center',
+                }}
+              />
+              Championship
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignContent: 'center',
+                color: '#ABADB2',
+              }}
+            >
+              04/05, 22:00
+            </div>
+          </Card.Header>
+          <Card.Body
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Card.Title>
+              <div>Luton</div>
+              <div>Rotherham</div>
+            </Card.Title>
+            <Card.Text>
+              <div
+                style={{
+                  backgroundColor: '#1A2E37',
+                  border: 'none',
+                  pointer: 'cursor',
+                  padding: '.2rem',
+                  width: '100%',
+                }}
+              >
+                <div
+                  style={{
+                    padding: '.2rem',
+                  }}
+                >
+                  Market: 3 Away
+                </div>
+                <div
+                  style={{
+                    padding: '.2rem',
+                    color: 'green',
+                  }}
+                >
+                  Pick: Luton
+                </div>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <Container
+        style={{
+          marginTop: '8rem',
+          // backgroundColor: 'yellow',
+          paddingLeft: '2rem',
+          paddingRight: '2rem',
+        }}
+      >
+        <h4>Deposit</h4>
+        <Nav className="justify-content-center" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link
+              href="#"
+              style={{
+                borderRadius: '30%',
+                backgroundColor: '#1A2E37',
+                display: 'flex',
+                flexDirectiom: 'column',
+                justifycontent: 'center',
+                textAlign: 'center',
+                marginLeft: '.5rem',
+                marginRight: '.5rem',
+              }}
+            >
+              <div>
+                <div
+                  style={{
+                    fontSize: '.8rem',
+                    alignSelf: 'center',
+                    color: 'green',
+                  }}
+                >
+                  1 Day
+                </div>
+                <div
+                  style={{
+                    fontSize: '1rem',
+                    color: 'white',
+                    alignSelf: 'center',
+                  }}
+                >
+                  KSh 30
+                </div>
+              </div>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              href="#"
+              style={{
+                borderRadius: '30%',
+                backgroundColor: '#1A2E37',
+                display: 'flex',
+                flexDirectiom: 'column',
+                justifycontent: 'center',
+                textAlign: 'center',
+                marginLeft: '.5rem',
+                marginRight: '.5rem',
+              }}
+            >
+              <div>
+                <div
+                  style={{
+                    fontSize: '.8rem',
+                    alignSelf: 'center',
+                    color: 'green',
+                  }}
+                >
+                  Weekly
+                </div>
+                <div
+                  style={{
+                    fontSize: '1rem',
+                    color: 'white',
+                    alignSelf: 'center',
+                  }}
+                >
+                  KSh 200
+                </div>
+              </div>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              href="#"
+              style={{
+                borderRadius: '30%',
+                backgroundColor: '#1A2E37',
+                display: 'flex',
+                flexDirectiom: 'column',
+                justifycontent: 'center',
+                textAlign: 'center',
+                marginLeft: '.5rem',
+                marginRight: '.5rem',
+              }}
+            >
+              <div>
+                <div
+                  style={{
+                    fontSize: '.8rem',
+                    alignSelf: 'center',
+                    color: 'green',
+                  }}
+                >
+                  Monthly
+                </div>
+                <div
+                  style={{
+                    fontSize: '1rem',
+                    color: 'white',
+                    alignSelf: 'center',
+                  }}
+                >
+                  KSh 500
+                </div>
+              </div>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
     </div>
   );
 };
